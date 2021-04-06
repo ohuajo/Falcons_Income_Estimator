173ff352196a3739a06403bef32ff3ac506d8446
mike_S_home <mike.l.salzarulo@gmail.com>
2021-04-06 15:03:56 -0400

    updated styles

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
 mpl.RcParams.update({"figure.facecolor": "k", "axes.edgecolor": "w"})
 
 
@@ -64,9 +65,25 @@ def main():
     # ====
 
     # show histograms of income and age
-    df.hist(column="income", bins=2)
-    df.hist(column="age")
-    plt.show()
+    # income
+    fig, ax = plt.subplots()
+    rects = df.hist(column="income", bins=2, ax=ax, grid=False, ec="k")
+    # rects[-2].set_fc("r")
+    ax.patches[-1].set_fc("r")
+    ax.set_xticks(np.linspace(df.income.min(), df.income.max(), 3, dtype=np.uint8))
+    ax.set_xlabel("Income Bin")
+    ax.set_ylabel("Number in bin")
+    ax.set_title("Distribution of Income")
+    plt.show(block=False)
+
+    # age
+    fig, ax = plt.subplots()
+    rects = df.hist(column="age", ax=ax, grid=False, ec="k")
+    ax.set_xticks(np.linspace(df.age.min(), df.age.max(), 11, dtype=np.uint8))
+    ax.set_xlabel("Age Bin")
+    ax.set_ylabel("Number in bin")
+    ax.set_title("Distribution of Age")
+    plt.show(block=False)
 
     # This was tricky to view categorical data as a single variable
     # I needed to unpack the catagories and the count of each occurance
@@ -74,8 +91,8 @@ def main():
     heights = df.workingclass.value_counts().values
 
     # some fancy formatting and then basic plotting
-    fig, ax = plt.subplots(figsize=(10, 12))
-    fig.subplots_adjust(left=0.13, right=.99, top=.99, bottom=.05)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    fig.subplots_adjust(left=0.13, right=.99, top=.89, bottom=.05)
     rects = ax.barh(np.arange(len(heights)), heights, tick_label=labels)
 
     # Due to the skew of categories the visualization isn't very telling
@@ -86,13 +103,17 @@ def main():
         value = heights[ind]
         # These values are heuristically determined based on the data
         xloc = 5
+        color = "k"
         if width > 22000:
             xloc = -50
+            color = "w"
         yloc = rect.get_y() + rect.get_height() / 2
         # plot the annotations with appropriate formatting
         label = ax.annotate(value, xy=(width, yloc), xytext=(xloc, 0), textcoords="offset points",
-                            verticalalignment='center', color="k", clip_on=True, weight="bold")
+                            verticalalignment='center', color=color, clip_on=True, weight="bold")
         rect_labels.append(label)
+
+    ax.set_title("Distribution of People by Workclass")
     plt.show()
 
     # ====
